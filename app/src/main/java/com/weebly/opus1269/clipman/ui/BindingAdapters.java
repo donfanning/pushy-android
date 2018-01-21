506c70b10006a0eb1c572b1b089e0b5d2b274aa2
opus1269 <igreilly1269@gmail.com>
2018-01-21 10:23:43 -0600

    add BindingAdapters

---
+++ app/src/main/java/com/weebly/opus1269/clipman/ui/BindingAdapters.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2016-2017, Michael A. Updike All rights reserved.
+ * Licensed under Apache 2.0
+ * https://opensource.org/licenses/Apache-2.0
+ * https://github.com/Pushy-Clipboard/pushy-android/blob/master/LICENSE.md
+ */
+
+package com.weebly.opus1269.clipman.ui;
+
+import android.databinding.BindingAdapter;
+import android.view.View;
+
+/** Data binding utility methods */
+public class BindingAdapters {
+  @BindingAdapter("visibleGone")
+  public static void showHide(View view, boolean show) {
+    view.setVisibility(show ? View.VISIBLE : View.GONE);
+  }
+}
